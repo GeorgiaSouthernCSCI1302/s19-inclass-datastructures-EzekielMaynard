f108ffd879c251dbb174107ca7239816667a0dfa
em04743 <em04743@CEIT2553214X018.ad.georgiasouthern.edu>
2019-03-27 17:09:27 -0400

    Work for 3/27/2019

---
+++ InClassDataStuctures/src/MyIV.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+
+/**
+ * @author em04743
+ *
+ */
+
+import javafx.scene.image.ImageView;
+
+public class MyIV extends ImageView {
+
+	private int value;
+	private String suit;
+	
+	private void R() {
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
